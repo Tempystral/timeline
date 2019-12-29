3608c5df0e7ad3f73ebe76e2cd89714fc641f629
Tempystral <elijah.doern@gmail.com>
2019-12-29 17:31:39 -0500

    Update create-timeline.js

--- scripts/create-timeline.js
+++ scripts/create-timeline.js
@@ -1,5 +1,6 @@
 var spreadsheet = 'https://docs.google.com/spreadsheets/d/15oJFkwp6unPc9JFPoc5mhgfMWcvt6E3dZRu8xUE0xoY/edit?usp=sharing'
 var options = {
-  timenav_height_percentage: 40
+  timenav_height_percentage: 40,
+  initial_zoom: 3
 }
 timeline = new TL.Timeline('timeline-embed', spreadsheet, options);
