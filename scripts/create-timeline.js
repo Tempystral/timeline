fe64a69d43d918b5dc5465564527caacf9fbbe6d
Tempystral <elijah.doern@gmail.com>
2020-01-02 19:42:21 -0500

    Updated CSS

--- scripts/create-timeline.js
+++ scripts/create-timeline.js
@@ -1,6 +1,7 @@
 var spreadsheet = 'https://docs.google.com/spreadsheets/d/15oJFkwp6unPc9JFPoc5mhgfMWcvt6E3dZRu8xUE0xoY/edit?usp=sharing'
+var sheetjson = ''
 var options = {
-  timenav_height_percentage: 40,
+  timenav_height_percentage: 30,
   initial_zoom: 3,
   hash_bookmark: true
 }
